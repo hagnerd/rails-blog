352b135eae798a182c4faf4e8ed50f255d49c5b5
Matt Hagner <web@matthagner.com>
2018-09-30 17:02:53 -0500

    Tagging feature complete

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,4 +2,17 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
   end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    flash[:notice] = "Succesfully deleted tag '#{@tag.name}'"
+
+    redirect_to tags_path
+  end
 end
